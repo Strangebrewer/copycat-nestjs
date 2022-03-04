9d153345e85b6bc8f8cc1fd3d9d0a2fa989e8412
Strangebrewer <BKAShambala@gmail.com>
2022-03-03 23:17:45 -0700

    setup user and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig]}),
+    CommonModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/rememe')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
